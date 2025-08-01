4a06073560cade5a11e0573e40d188fdd09f469e
yangsi <obpt@sina.com>
2025-08-01 22:09:56 +0800

    移除过时的文件

--- gulp/default.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const log = require('fancy-log');
-gulp.task('default', function () {
-    log.info("==============USAGES===================")
-    for (taskName in gulp.tasks) {
-        log.info(`gulp ${taskName}`);
-    }
-    log.info("=======================================")
-});
